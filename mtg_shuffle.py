fb613671bdc5585d39bfe5c9f0e8df97baa5fc61
nickubed <nhpquandt@gmail.com>
2020-09-19 19:11:11 -0700

    adapted to be more OOP-y

--- mtg_shuffle.py
+++ mtg_shuffle.py
@@ -1,32 +1,35 @@
 import random
-count = 0
-merged = []
-def mtg_shuffle(ls):
-    global count
-    global merged
-    piles = []
-    while count < 5:
 
-        for _ in range(10 if len(ls) > 10 else len(ls)):
-            piles.append([])
-        while len(ls) > 0:
+class MtgShuff:
+    def __init__(self, count = 0, merged = []):
+        self.count = count
+        self.merged = merged
+    def mtg_shuffle(self, ls):
+        piles = []
+        while self.count < 5:
+
+            for _ in range(10 if len(ls) > 10 else len(ls)):
+                piles.append([])
+            while len(ls) > 0:
+                for p in piles:
+                    for _ in ls:
+                        p.append(ls.pop(0))
+                        break
+                continue
+            
             for p in piles:
-                for _ in ls:
-                    p.append(ls.pop(0))
-                    break
-            continue
-        
-        for p in piles:
-            while len(p):
-                for _ in p:
-                    merged.append(p.pop(random.randint(0,len(p)-1)))
-                    break
-            continue
-        count += 1
-        mtg_shuffle(merged)
+                while len(p):
+                    for _ in p:
+                        self.merged.append(p.pop(random.randint(0,len(p)-1)))
+                        break
+                continue
+            self.count += 1
+            self.mtg_shuffle(self.merged)
 
-    return merged
+        return self.merged
 
 standard_deck = [{'Card': x} for x in range(60)]
 
-print(mtg_shuffle(standard_deck))
+shuff = MtgShuff()
+
+print(shuff.mtg_shuffle(standard_deck))
