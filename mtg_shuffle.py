d3ec5dcaf739a12a79aeb9d178707976870ff433
nickubed <nhpquandt@gmail.com>
2020-09-19 19:06:01 -0700

    cleaned up a touch

--- mtg_shuffle.py
+++ mtg_shuffle.py
@@ -16,7 +16,6 @@ def mtg_shuffle(ls):
                     break
             continue
         
-        print(piles)
         for p in piles:
             while len(p):
                 for _ in p:
@@ -28,5 +27,6 @@ def mtg_shuffle(ls):
 
     return merged
 
+standard_deck = [x for x in range(60)]
 
-print(mtg_shuffle([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40]))
+print(mtg_shuffle(standard_deck))
